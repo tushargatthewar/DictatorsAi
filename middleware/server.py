bc9f664117e3a85ebada8bcca22d89b5de559908
Tushar Santosh Gatthewar <tushargatthewar73@gmail.com>
2026-01-02 14:33:54 +0530

    commit

--- middleware/server.py
+++ middleware/server.py
@@ -1632,5 +1632,6 @@ def feedback(u):
 
 
 
-if __name__ == '__main__':
-    app.run(port=5000, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
